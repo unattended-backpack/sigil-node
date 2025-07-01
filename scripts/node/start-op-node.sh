5c65e19d1191a65c6b756fa5d3441389ac56b759
jossduff <jduff360@gmail.com>
2025-07-01 15:43:13 -0400

    remove sequencer flags for rpc node

--- scripts/node/start-op-node.sh
+++ scripts/node/start-op-node.sh
@@ -12,9 +12,7 @@ op-node \
 	--l2.jwt-secret=/shared/jwt.txt \
 	--l1.beacon=$L1_BEACON_RPC_URL \
 	--l2.enginekind=geth \
-	--sequencer.enabled \
 	--verifier.l1-confs=64 \
 	--rollup.config=/chainconfig/rollup.json \
 	--rpc.enable-admin \
-	--p2p.sequencer.key=$GS_SEQUENCER_PRIVATE_KEY \
 	--safedb.path=/safedb
